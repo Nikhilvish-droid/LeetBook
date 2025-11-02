ce69a3a07b4758ec126d6430043fc7ea8099f6d1
Nikhil vishwakarma <nikhil.vish847@gmail.com>
2025-11-02 12:25:11 +0530

    Time: 343 ms (69.39%), Space: 39.2 MB (68.37%) - LeetHub

---
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
+        mat = [[0] * n for _ in range(m)]
+
+        for x, y in walls:
+            mat[x][y] = -1  
+        for x, y in guards:
+            mat[x][y] = 2  
+
+        def watch(x, y):
+            for c in range(y + 1, n):
+                if mat[x][c] == -1 or mat[x][c] == 2:
+                    break
+                mat[x][c] = 1
+            for c in range(y - 1, -1, -1):
+                if mat[x][c] == -1 or mat[x][c] == 2:
+                    break
+                mat[x][c] = 1
+            for r in range(x + 1, m):
+                if mat[r][y] == -1 or mat[r][y] == 2:
+                    break
+                mat[r][y] = 1
+            for r in range(x - 1, -1, -1):
+                if mat[r][y] == -1 or mat[r][y] == 2:
+                    break
+                mat[r][y] = 1
+
+        for x, y in guards:
+            watch(x, y)
+
+        # Count unguarded cells
+        ans = 0
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    ans += 1
+        return ans
