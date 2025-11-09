2bac895e7f52d7da70f5b6708ce599479aa7c467
Nikhil vishwakarma <nikhil.vish847@gmail.com>
2025-11-09 14:38:57 +0530

    Time: 6 ms (48.35%), Space: 25.6 MB (47.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,10 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        length= len(nums)
-        k= k%length
-        for i  in range(k):
-            nums.insert(0,nums[-1])
-            nums.pop(-1)
-        return nums
+        n= len(nums)
+        k= k%n
+        rotated=[0]*n
+        for i in range(n):
+            rotated[(i+k)%n]=nums[i]
+        
+        for i in range(n):
+            nums[i]=rotated[i]
