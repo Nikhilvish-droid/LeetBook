4a6dd16168f012221843862957f23435a2b80026
Nikhil vishwakarma <nikhil.vish847@gmail.com>
2026-07-11 12:11:53 +0530

    Time: 0 ms (100%), Space: 48.8 MB (50.43%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,27 +1,35 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int k = 0;
-        int curr = nums[0];
-        int curridx = 1;
-        int count = 1;
-        for(int i = 1; i < nums.length; i++){
+        // int curr = nums[0];
+        // int curridx = 1;
+        // int count = 1;
+        // for(int i = 1; i < nums.length; i++){
         
-            if(nums[i] == curr){
-                if(count < 2){
-                    nums[curridx] = nums[i];
-                    curridx++;
-                    count++;
-                }
-            }
-            else{
-                nums[curridx] = nums[i];
-                curridx++;
-                count = 1;
-                curr = nums[i];
-            }
+        //     if(nums[i] == curr){
+        //         if(count < 2){
+        //             nums[curridx] = nums[i];
+        //             curridx++;
+        //             count++;
+        //         }
+        //     }
+        //     else{
+        //         nums[curridx] = nums[i];
+        //         curridx++;
+        //         count = 1;
+        //         curr = nums[i];
+        //     }
             
-        }
-        return curridx;
+        // }
+        // return curridx;
 
+        //or two pointer approach
+        int k = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(k < 2 || nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
 }
